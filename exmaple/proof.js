fd23ff152594b9907ce126597c376202ae5f32af
leihuhu <lei.huhu@163.com>
2025-03-14 18:20:30 +0800

    fix: proof example

--- exmaple/proof.js
+++ exmaple/proof.js
@@ -6,24 +6,33 @@ const { dataview } = require('@dioxide-js/misc')
 
 const web3 = new Web3('http://localhost:7600')
 
-const user1 = {
-  PrivateKey: 'gMAsFkh3C6Q63XAd+MoZC7BUrQTCAi8DAEzHGDXJOqc=',
-  PublicKey: '/TbfaenGkDtwrcM6WmCJ2BMzeQ3MXjlBd4IDQ6QEJYDBLuVFh+niU8kocEJEDZQp1Tp7H69VG3W9W2iSZhHPoQ==',
-  Address: '795csryp16ep27cwbhqnj510ddkv904n961kat1tm2vswp37xf878by600:sm2',
-}
-
-const user2 = {
-  PrivateKey: 'QYm4dkdD1zsN/0pui4JUj+c9dJVPFk16ifk8z0m+9og=',
-  PublicKey: 'BLJZvVfYw29Xb8qu3FfdaeCR1X3v3hfbX+Dw98TPULSJ4swqFYpTvxrHKOGA2r2w2YqXG4ceL0YSbfR0wBW7Fp8=',
-  Address: 'gjt11z0467ydw2a0bbqpbcwbbfp03p8xqvsfqcxfkkk3vf3yw6cj9ym730:sm2',
+const user = {
+  PrivateKey: 'emWeGPBrrail1xFqMhc5Omk6APyU4Wou/T8zBaBwEKM=',
+  PublicKey: 'u22RPIe1BNWo6ckxFj+UEpg4/15/jOsrJijmkq1+ey20yWeVKac60CDnqQ8jmu8qu7RQYMpHM4vVHo4BSuc0Ow==',
+  Address: 's62t06e3kp9192spw39djj231v9ftbh5wvvs3akyc6ytxrv02zr89d5d5w:sm2',
 }
 
 web3.proof
   .newProof({
-    sender: user2.Address,
-    secretKey: dataview.base64ToU8(user2.PrivateKey),
-    key: 'test_sdk',
-    content: 'test_sdk',
+    sender: user.Address,
+    secretKey: dataview.base64ToU8(user.PrivateKey),
+    key: 'test_sdk11',
+    content: 'test_sdk11',
   })
-  .then(console.log)
+  .then(async (tx_hash) => {
+    console.log(tx_hash)
+    await sleep(20000)
+    web3.proof
+      .getProofs({ tx_hash: tx_hash })
+      .then((proofs) => {
+        console.log(proofs)
+        web3.proof.checkProof({ proof_hash: proofs[0].ProofHash }).then(console.log).catch(console.error)
+      })
+      .catch(console.error)
+  })
   .catch(console.error)
+
+web3.proof.getProofs({ owner: user.Address }).then(console.log).catch(console.error)
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
--- src/api/proof.ts
+++ src/api/proof.ts
@@ -3,13 +3,13 @@ import { GetProofsParams, Proof } from './type'
 
 class ProofSvc extends Request {
   async getProofs(params: GetProofsParams): Promise<Proof[]> {
-    const resp = await this.post<Proof[]>('proof.get', params)
+    const resp = await this.post<Proof[]>('proofs.get', params)
     return resp
   }
 
   async checkProof(hash: string): Promise<boolean> {
-    const resp = await this.post<{ Result: boolean }>('proof.check', { hash })
-    return resp.Result
+    const resp = await this.post<boolean>('proofs.check', hash)
+    return resp
   }
 }
 
