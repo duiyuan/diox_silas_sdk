a873f82e3d7644079530ce71c64570f4dd6a4dd8
189 <189>
2025-04-12 15:44:16 +0800

    chore: esmodule

---
+++ rollup.config.mjs
@@ -0,0 +1,78 @@
+import { createRequire } from 'node:module'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
+
+const require = createRequire(import.meta.url)
+const pkg = require('./package.json')
+const plugs = [
+  json(),
+  resolve({
+    preferBuiltins: false,
+  }),
+  commonjs(),
+]
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+export default [
+  // ESM
+  {
+    input: 'src/index.ts',
+    output: {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+    plugins: [
+      ...plugs,
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: 'dist/esm/types',
+        outDir: 'dist/esm',
+      }),
+    ],
+  },
+
+  // CommonJS
+  // {
+  //   input: 'src/index.ts',
+  //   output: {
+  //     file: pkg.main,
+  //     format: 'cjs',
+  //     sourcemap: true,
+  //     exports: 'auto',
+  //   },
+  //   plugins: [
+  //     ...plugs,
+  //     typescript({
+  //       tsconfig: './tsconfig.json',
+  //       declaration: false,
+  //       outDir: 'dist/cjs',
+  //     }),
+  //   ],
+  // },
+  // {
+  //   input: 'src/index.ts',
+  //   output: {
+  //     file: pkg.browser,
+  //     format: 'umd',
+  //     name: 'DSSWeb3',
+  //     sourcemap: true,
+  //   },
+  //   plugins: [
+  //     resolve(),
+  //     commonjs(),
+  //     typescript({
+  //       tsconfig: './tsconfig.json',
+  //       declaration: false,
+  //       outDir: 'dist/umd',
+  //     }),
+  //     terser(),
+  //   ],
+  // },
+]
